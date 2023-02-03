b1796488b381c87f4d78e115e32db6fa5bf71ef8
watering_penguins <a18982024257@163.com>
2023-02-03 13:49:13 +0800

    modified:   "\346\225\260\346\215\256\347\273\223\346\236\204/\345\271\263\350\241\241\346\240\221/\343\200\214\346\250\241\346\235\277\343\200\215_Splay.cpp"

--- 数据结构/平衡树/「模板」_Splay.cpp
+++ 数据结构/平衡树/「模板」_Splay.cpp
@@ -48,7 +48,7 @@ inline int qmi(int x,int y,int mod){
 }
 inline int dc1(int x) {return x*(x+1)/2;}
 inline int dc2(int x) {return x*(x+1)*(x+x+1)/6;}
-int fa[N],cnt[N],val[N],son[N][2],si[N],root;
+int fa[N],cnt[N],val[N],son[N][2],si[N],root,tot;
 inline bool getson(int x){
     return son[fa[x]][1]==x;
 }
@@ -56,19 +56,72 @@ inline void pu(int x){
     si[x]=cnt[x]+si[son[x][0]]+si[son[x][1]];
     return ;
 }
+inline int bt(){
+	tot++;
+	son[tot][0]=son[tot][1]=fa[tot]=0;
+	si[tot]=cnt[tot]=1;
+	return tot;
+}
 void rotate(int x){
     int fath=fa[x],gfath=fa[fath],gs=getson(x);
     son[fath][gs]=son[x][gs^1]; //now.~gsson -> fa.gxson 
     fa[son[fath][gs]]=fath;
-    son[x][gs^1]=fath;          //
+    son[x][gs^1]=fath;          //fa -> now.~gsson
     fa[fath]=x;
-    fa[x]=gfath;
-    if(gfath){
-        son[gfath][son[gfath][1]==fath]=x;
-    }
+    fa[x]=gfath;				//granfa -> now.fa
+    if(gfath)son[gfath][son[gfath][1]==fath]=x;
     pu(fath);
     pu(x);
     return ;
+}
+inline void splay(int x){
+	for(int fath;fath=fa[x];rotate(x)){
+		if(fa[fath]){
+			rotate((getson(x)==getson(fath)?fath:x));
+		}
+	}
+	root=x;
+}
+inline void insert(int x){
+	if(!root){
+		int now=bt();
+		root=now;
+		val[now]=x;
+		return ;
+	}
+	int now=root,fath=0;
+	while(1){
+		if(x==val[now]){
+			cnt[now]++;
+			pu(now);
+			pu(fath);
+			splay(now);
+			break;
+		}
+		fath=now;
+		now=son[now][val[now]<x];
+		if(!now){
+			now=bt();
+			fa[now]=fath;
+			son[fath][val[fath]<x]=now;
+			val[now]=x;
+			pu(fath);
+			splay(now);
+			break;
+		}
+	}
+	return ;
+}
+inline int kth(int x){
+	int now=root;
+	while(1){
+		if(son[now][0]&&x<=si[son[now][0]])now=son[now][0];
+		else{
+			int tmp=(son[now][0]?si[son[now][0]]:0)+cnt[now];
+			if(x<=tmp)
+		}
+
+	}
 }
 signed main(){
     
